2e9c8f06d241103429101086959769d5b1696523
zhangyanru <zhangyanru@wshifu.com>
2024-04-23 18:35:37 +0800

    feat: 状态选中

--- packages/components/panelSelect/src/panelSelect.vue
+++ packages/components/panelSelect/src/panelSelect.vue
@@ -2,14 +2,14 @@
  * @Author: zhangyanru zhangyanru@wshifu.com
  * @Date: 2024-04-15 14:39:08
  * @LastEditors: zhangyanru zhangyanru@wshifu.com
- * @LastEditTime: 2024-04-19 16:43:38
+ * @LastEditTime: 2024-04-23 18:05:20
  * @FilePath: /vue3-component/packages/components/regionSelect/src/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
   <div class="region-box">
     <div class="region-box-title border-bottom">
-      选择县/区，已选择<span style="color: #409EFF;">{{ selectData.length || 0 }}</span>个
+      选择县/区，已选择<span style="color: #409EFF;">{{ _selectData.length || 0 }}</span>个
     </div>
     <div class="flex h40 border-bottom">
       <div class="w170 text-align-left border-right pl-10 border-box">省</div>
@@ -27,25 +27,32 @@
         >
           <div class="flex justify-space-between plr-10 h30 pointer">
             <div>{{ province.name }}</div>
-            <div>0</div>
+            <div style="color: #409EFF">
+              {{ province.children?.reduce((pre,next) => {
+                return pre + ( next.children? next.children.filter(t => t.checked).length : 0)
+              }, 0) }}
+            </div>
           </div>
         </div>
       </div>
       <!-- 市 -- 区/县 -->
       <div class="flex-1 overflow-y-scroll">
-        <div v-for="city in formatData[activeProvinceIndex].childList" :key="city.id" class="flex border-bottom">
+        <div v-for="(city, cityIndex) in formatData[activeProvinceIndex].children" :key="city.id" class="flex border-bottom">
           <div class="flex w240">
             <div class="w240 flex justify-space-between border-right align-item-center plr-10">
               <div class="flex align-item-center">
-                <el-checkbox v-model="city.checked" />
+                <el-checkbox v-model="city.checked" @change="cityChange($event, cityIndex, city)" />
                 <div class="ml-5">{{ city.name }}</div>
               </div>
-              <div>0/1</div>
+              <div>
+                <span style="color: #409EFF">{{ city.children ? city.children.filter(item => item.checked).length : 0 }}</span>
+                /<span>{{ city.children ? city.children.length : 0 }}</span>
+              </div>
             </div>
           </div>
           <div class="flex flex-wrap flex-1 pl-10">
-            <div v-for="area in city.childList" :key="area.id" class="flex h45 mr-15 align-item-center">
-              <el-checkbox v-model="area.checked" />
+            <div v-for="(area, areaIndex) in city.children" :key="area.id" class="flex h45 mr-15 align-item-center">
+              <el-checkbox v-model="area.checked" @change="areaChange($event, cityIndex, area)" />
               <div class="ml-5">{{ area.name }}</div>
             </div>
           </div>
@@ -57,9 +64,9 @@
 
 <script setup lang="ts">
 import './panelSelect.scss'
-import { ref, defineProps, computed } from 'vue'
-import { panelSelectProps, DataType, Id } from './panelSlect'
-import { ElCheckbox } from 'element-plus'
+import { ref, defineProps, reactive, watch } from 'vue'
+import { panelSelectProps, DataType, Id, SelectDataType } from './panelSlect'
+import { ElCheckbox, CheckboxValueType } from 'element-plus'
 import 'element-plus/dist/index.css'
 
 defineOptions({
@@ -67,12 +74,27 @@ defineOptions({
 })
 
 //props
-const { data, labelField, keyField, childrenField, selectData } = defineProps(panelSelectProps)
-
+const { data, labelField, keyField, childrenField, selectData, } = defineProps(panelSelectProps)
+// 当前选中的省份
 const activeProvinceIndex = ref(0)
 
-const formatData = computed(() => {
-  return formatInitData(data)
+// 选中的县区
+let _selectData = reactive<Id[]>(selectData.map((t: SelectDataType) => (t.id)))
+// 最终的渲染数据
+let formatData = reactive<DataType[]>(formatInitData(data))
+
+// watch(
+//   [
+//     _selectData, 
+//     (newVal, oldVal) => {
+//       formatData = formatInitData(data)
+//     }, 
+//     { deep: true }
+//   ], 
+// )
+watch([_selectData, formatData], ([newSelectData, newFormatData], [oldSelectData, oldFormatData]) => {
+  formatData = formatInitData(data)
+   
 })
 
 // 格式化传入的data数据
@@ -82,8 +104,9 @@ function formatInitData(data: DataType[]) {
       return {
         id: t[keyField] as Id,
         name: t[labelField] as string,
-        checked: selectData.includes(t[keyField] as Id),
-        childList: t[childrenField] ? track(t[childrenField]) : []
+        checked: _selectData.includes(t[keyField] as Id),
+        children: t[childrenField] ? track(t[childrenField]) : [],
+        isLast: !t[childrenField]
       }
     })
   }
@@ -91,7 +114,39 @@ function formatInitData(data: DataType[]) {
 }
 
 // 选中市
+function cityChange(e: CheckboxValueType, cityIndex: number, city: DataType) {
+  const res = formatData[activeProvinceIndex.value].children[cityIndex].children as DataType[]
+  const ls: Id[] = res.map(t => (t.id))
+  ls.push(city.id)
+  if (e) {
+    for(const t of ls) {
+      _selectData.push(t)
+    }
+  } else {
+    for (const t of ls) {
+      if (_selectData.indexOf(t) > -1) {
+        _selectData.splice(_selectData.indexOf(t), 1)
+      }
+    }
+  }
+}
 
 // 选中区县
+function areaChange(e: CheckboxValueType, cityIndex: number, area: DataType) {
+  if (e) {
+    _selectData.push(area.id)
+  } else {
+    _selectData.splice(_selectData.indexOf(area.id), 1)
+  }
+  // 检查是否需要选中市
+  const itemLs = formatData[activeProvinceIndex.value].children[cityIndex].children.length
+  const selectLs = formatData[activeProvinceIndex.value].children[cityIndex].children.filter(t => t.checked).length
+  console.log('formatData ', formatData[activeProvinceIndex.value].children[cityIndex]);
+  // if (itemLs === selectLs) {
+  //   formatData[activeProvinceIndex.value].children[cityIndex].checked = true
+  // } else {
+  //   formatData[activeProvinceIndex.value].children[cityIndex].checked = false
+  // }
+}
 
-</script>./panelSlect
+</script>
--- packages/components/panelSelect/src/panelSlect.ts
+++ packages/components/panelSelect/src/panelSlect.ts
@@ -2,7 +2,7 @@
  * @Author: zhangyanru zhangyanru@wshifu.com
  * @Date: 2024-04-15 14:39:15
  * @LastEditors: zhangyanru zhangyanru@wshifu.com
- * @LastEditTime: 2024-04-19 16:31:10
+ * @LastEditTime: 2024-04-22 17:27:28
  * @FilePath: /vue3-component/packages/components/regionSelect/src/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -18,7 +18,7 @@ export const panelSelectProps = {
   // 选中的数据
   selectData: {
     type: Array as PropType<SelectDataType[]>,
-    default: () => [640300]
+    default: () => []
   },
   labelField: {
     type: String,
@@ -30,7 +30,7 @@ export const panelSelectProps = {
   },
   childrenField: {
     type: String,
-    default: 'childList'
+    default: 'children'
   }
 } as const
 
@@ -50,7 +50,7 @@ export type DataType = {
 }
 
 // 传入的selectData类型
-export type SelectDataType = Id | {
+export type SelectDataType = {
   id: Id;
   name?: string;
   parentId?: Id;
